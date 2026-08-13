4a193c9d3481bb949b41915eeeb8d3975b4e5fac
Akshay Kumar <84731128+a4kshay@users.noreply.github.com>
2026-08-13 20:57:54 +0530

    Clarify initial pointer value with a comment
    Added a comment to clarify the initial value of ptr.

--- Problems/27-remove-element/27-Remove-Element.java
+++ Problems/27-remove-element/27-Remove-Element.java
@@ -1,7 +1,7 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
         int n=nums.length;
-        int ptr =0;
+        int ptr =0;   // index number initially take 0 
 
         for(int i=0; i<nums.length; i++){
 
